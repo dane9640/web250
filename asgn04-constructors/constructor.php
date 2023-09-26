364fc332fcd00b3bc1e2fe37204a8dd8e185b16c
dane9640 <gandalf12298@gmail.com>
2023-09-26 15:56:35 -0400

    Completed Bird 6.1 Bird Class

---
+++ asgn04-constructors/constructor.php
@@ -0,0 +1,25 @@
+<?php
+
+class Bird {
+    public $commonName;
+    public $latinName;
+
+    public function __construct($commonName, $latinName) {
+        $this->commonName = $commonName;
+        $this->latinName = $latinName;
+    }
+
+    public function displayInfo() {
+        echo "Common Name: " . $this->commonName . "<br>Latin Name: " . $this->latinName . "<br><br>";
+    }
+}
+
+// Creating two instances of the Bird class
+$robin = new Bird('Robin', 'Turdus migratorius');
+$easternTowhee = new Bird('Eastern Towhee', 'Pipilo erythrophthalmus');
+
+// Optional: print out the details
+$robin->displayInfo();
+$easternTowhee->displayInfo();
+
+?>
