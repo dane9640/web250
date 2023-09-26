36db233df5a1e34750e24d4804575e4939ad348d
dane9640 <gandalf12298@gmail.com>
2023-09-26 16:33:02 -0400

    Finished Autoloader Assignment

---
+++ asgn04-constructors/autoload.php
@@ -0,0 +1,15 @@
+<?php
+
+function my_autoloader($class) {
+  if(preg_match('/\A\w+\Z/', $class)) {
+    include 'classes/' . $class . '.class.php';
+  }
+}
+
+spl_autoload_register('my_autoloader');
+
+$flyCatcher = new Bird(['commonName' =>'Acadian Flycatcher', 'latinName' => 'Empidonax Virescens']);
+
+$flyCatcher->displayInfo();
+
+?>
