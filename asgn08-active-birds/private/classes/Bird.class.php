9fcb1751bbaac0b33aae9a93bda03a1dd4f104e8
dane9640 <gandalf12298@gmail.com>
2023-10-24 17:40:30 -0400

    Finished assignment 8

--- asgn08-active-birds/private/classes/Bird.class.php
+++ asgn08-active-birds/private/classes/Bird.class.php
@@ -2,15 +2,63 @@
 
 class Bird {
 
-/*
-Use the wnc-birds.csv file to create the properties
-Make all of the properties public.
-*/
+static protected $database;
+
+static public function setDatabase($database) {
+  self::$database = $database;
+}
+
+static public function findBySQL($sql) {
+  $result = self::$database->query($sql);
+
+  if(!$result) {
+    exit("Database query failed.");
+  }
+
+  $objectArray = [];
+  while($record = $result->fetch_assoc()) {
+    $objectArray[] = self::instantiate($record);
+  }
+
+  $result->free();
+  return $objectArray;
+}
+
+static public function findAll() {
+  $sql = "SELECT * FROM birds";
+  $result = self::findBySQL($sql);
+  return $result;
+}
+
+static public function findByID($id) {
+  $sql = "SELECT * FROM birds ";
+  $sql .= "WHERE id='" . self::$database->escape_string($id) . "'";
+
+  $objectArray = self::findBySQL($sql);
+  if(!empty($objectArray)) {
+    return array_shift($objectArray);
+  }
+  else {
+    return false;
+  }
+}
+
+static protected function instantiate($record) {
+  $object = new self;
+
+  foreach($record as $property => $value) {
+    if(property_exists($object, $property)) {
+      $object->$property = $value;
+    }
+  }
+
+  return $object;
+}
+
+  public $id;
   public $common_name;
   public $habitat;
   public $food;
-  public $nest_placement;
-  public $behavior;
   public $backyard_tips;
   public $conservation_id;
 
@@ -39,11 +87,10 @@ Make all of the properties public.
    - Create a default value of 1 for conservation_id
  */
   public function __construct($args=[]) {
+    $this->id = $args['id'] ?? '';
     $this->common_name = $args['common_name'] ?? '';
     $this->habitat = $args['habitat'] ?? '';
     $this->food = $args['food'] ?? '';
-    $this->nest_placement = $args['nest_placement'] ?? '';
-    $this->behavior = $args['behavior'] ?? '';
     $this->backyard_tips = $args['backyard_tips'] ?? '';
     $this->conservation_id = $args['conservation_id'] ?? 1;
   }
---
+++ asgn08-active-birds/public/birds.php
@@ -0,0 +1,43 @@
+<?php 
+  require_once('../private/initialize.php');
+  $page_title = 'Sightings';
+  include(SHARED_PATH . '/public_header.php');
+?>
+
+<h2>Bird inventory</h2>
+<p>This is a short list -- start your birding!</p>
+
+<table border="1">
+  <tr>
+    <th>Common Name</th>
+    <th>Habitat</th>
+    <th>Food</th>
+    <th>Conservation Status</th>
+    <th>Backyard Tips</th>
+    <th>Details</th>
+  </tr>
+   
+<?php
+
+  $birds = Bird::findAll();
+
+  foreach($birds as $bird) { 
+?>
+
+  
+  
+<tr>
+  <td><?php echo h($bird->common_name) ?></td>
+  <td><?php echo h($bird->habitat) ?></td>
+  <td><?php echo h($bird->food) ?></td>
+  <td><?php echo h($bird->conservation_text()) ?></td>
+  <td><?php echo h($bird->backyard_tips) ?></td>
+  <td><a href="detail.php?id=<?php echo $bird->id ?>">View</a></td>
+</tr>   
+
+<?php } ?>
+
+</table>
+
+
+<?php include(SHARED_PATH . '/public_footer.php'); ?>
