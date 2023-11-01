855bb3dff818817c1efe10e6267ceee92aeaca78
dane9640 <gandalf12298@gmail.com>
2023-10-31 21:17:54 -0400

    Finished Asgn09

---
+++ asgn09-CRUD/private/db-credentials.php
@@ -0,0 +1,11 @@
+<?php
+
+// FILEPATH: /C:/xampp/htdocs/web250/asgn07-oop-db-intro/private/db-credentials.php
+
+// Database credentials
+define('DB_SERVER', 'localhost');
+define('DB_USER', 'dane9640');
+define('DB_PASS', 'wd6ebwyw');
+define('DB_NAME', 'chain_gang');
+
+?>
